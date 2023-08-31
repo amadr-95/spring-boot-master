979d13c0a001daf89c1f6714d0c7c143049cf14d
Amador <amasabcar@outlook.com>
2023-08-31 18:40:44 +0200

    implement to interfaces

---
+++ src/main/java/com/project/customer/CustomerDAO.java
@@ -0,0 +1,8 @@
+package com.project.customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    List<Customer> getCustomers();
+}
--- src/main/java/com/project/customer/CustomerFakeRepository.java
+++ src/main/java/com/project/customer/CustomerFakeRepository.java
@@ -3,15 +3,16 @@ package com.project.customer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collections;
 import java.util.List;
 
 @Repository
-@Qualifier(value = "real")
-public class CustomerRepository implements CustomerDAO {
+@Qualifier(value = "fake")
+public class CustomerFakeRepository implements CustomerDAO {
     @Override
     public List<Customer> getCustomers() {
-        //TODO connect to real db
-        return Collections.emptyList();
+        return List.of(
+                new Customer(1L, "Amador"),
+                new Customer(2L, "Sandra")
+        );
     }
 }
