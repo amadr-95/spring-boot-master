9657617b4d6c9146ca9535221b84eb18fe0ccda8
Amador <amasabcar@outlook.com>
2023-09-01 19:12:02 +0200

    added custom exception

---
+++ src/main/java/com/project/exception/ApiRequestException.java
@@ -0,0 +1,14 @@
+package com.project.exception;
+
+public class ApiRequestException extends RuntimeException {
+    public ApiRequestException() {
+    }
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
