bbf07bb72aebe573d2b96959223e4c82edbb590a
Amador <amasabcar@outlook.com>
2023-09-02 18:24:13 +0200

    added annotations to persist in database and required no args constructor

--- src/main/java/com/project/customer/Customer.java
+++ src/main/java/com/project/customer/Customer.java
@@ -2,32 +2,43 @@ package com.project.customer;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
+@Table
 public class Customer {
 
+    @Transient
     private final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@([A-Za-z0-9.-]+\\.[A-Za-z]{2,})$";
 
-    private final Long id;
+    @Id
+    private Long id;
 
     @NotBlank(message = "name cannot be empty")
-    private final String name;
+    private String name;
 
     @NotBlank(message = "password cannot be empty")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private final String password;
+    private String password;
 
     //custom regex expression can be used instead of the default one
     @Email(regexp = EMAIL_PATTERN)
     @NotBlank(message = "email cannot be empty")
-    private final String email;
+    private String email;
 
     public Customer(Long id, String name, String password, String email) {
         this.id = id;
         this.name = name;
         this.password = password;
         this.email = email;
+    }
+
+    public Customer() {
     }
 
     @JsonProperty("customerId")
