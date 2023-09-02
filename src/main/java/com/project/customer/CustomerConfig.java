ef49725b60769c9de0f9141c367e7c097d86ae2b
Amador <amasabcar@outlook.com>
2023-09-02 13:51:47 +0200

    removed commented code

--- src/main/java/com/project/customer/CustomerConfig.java
+++ src/main/java/com/project/customer/CustomerConfig.java
@@ -1,16 +1,11 @@
 package com.project.customer;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CustomerConfig {
-
-    //this value is given by program arguments (false by default)
-    /*@Value("${app.useFakeCustomerRepo:false}")
-    private boolean useFakeCustomerRepo;*/
 
     @Bean
     CommandLineRunner commandLineRunner() {
@@ -19,9 +14,4 @@ public class CustomerConfig {
         };
     }
 
-   /* @Bean
-    CustomerDAO customerDAO() {
-        System.out.println("useFakeCustomerRepo = " + useFakeCustomerRepo);
-        return useFakeCustomerRepo ? new CustomerFakeRepository() : new CustomerRepository();
-    }*/
 }
