2049a534c445ba225af14a5675821b71ce474e47
Amador <amasabcar@outlook.com>
2023-09-03 19:30:58 +0200

    added equals and hashcode to be able to compare

--- src/main/java/com/project/customer/Customer.java
+++ src/main/java/com/project/customer/Customer.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Transient;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.Objects;
+
 @Entity
 @Table
 public class Customer {
@@ -57,6 +59,19 @@ public class Customer {
 
     public String getEmail() {
         return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
     }
 
     @Override
