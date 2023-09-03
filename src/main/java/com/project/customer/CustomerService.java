0e417c2d862a214680a45c9f465a53cbf9b878a0
Amador <amasabcar@outlook.com>
2023-09-03 10:54:45 +0200

    added logger

--- src/main/java/com/project/customer/CustomerService.java
+++ src/main/java/com/project/customer/CustomerService.java
@@ -1,6 +1,9 @@
 package com.project.customer;
 
 import com.project.exception.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,20 +11,28 @@ import java.util.List;
 @Service
 public class CustomerService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
     private final CustomerDAO customerDAO;
 
-    public CustomerService(CustomerDAO customerDAO) {
+    public CustomerService(@Qualifier("fake") CustomerDAO customerDAO) {
         this.customerDAO = customerDAO;
     }
 
     public List<Customer> getCustomers() {
+        LOGGER.info("calling getCustomers");
         return customerDAO.getCustomers();
     }
 
     public Customer getCustomer(Long customerId) {
         return customerDAO.findById(customerId)
                 .orElseThrow(
-                        () -> new ResourceNotFoundException("customer with id %s does not exist".formatted(customerId))
+                        () -> {
+                            ResourceNotFoundException resourceNotFoundException = new ResourceNotFoundException(
+                                    "customer with id %s does not exist".formatted(customerId));
+                            LOGGER.error("error in getting customer {}", customerId, resourceNotFoundException);
+                            return resourceNotFoundException;
+                        }
                 );
     }
 }
