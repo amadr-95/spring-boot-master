5ab12487695095d0a4f4df92291747c24dab4c96
Amador <amasabcar@outlook.com>
2023-09-03 13:07:28 +0200

    classes that brings specific data from the properties/yml file

--- src/main/java/com/project/customer/CustomerConfig.java
+++ src/main/java/com/project/customer/CustomerConfig.java
@@ -1,16 +1,37 @@
 package com.project.customer;
 
+import com.project.info.InfoApp;
+import com.project.info.InfoDB;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfig {
 
+    @Value("${info.app.name}")
+    private String appName;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+   /* private Environment environment;
+
+    public CustomerConfig(Environment environment) {
+        this.environment = environment;
+    }*/
+
     @Bean
-    CommandLineRunner commandLineRunner() {
+    CommandLineRunner commandLineRunner(InfoApp infoApp, InfoDB infoDB) {
         return args -> {
             System.out.println("Command line runner hooray");
+            System.out.println("appName = " + appName);
+            System.out.println("appVersion = " + appVersion);
+            //System.out.println(environment.getProperty("info.app.description"));
+            System.out.println("infoApp = " + infoApp);
+            System.out.println("infoDB = " + infoDB);
         };
     }
 
