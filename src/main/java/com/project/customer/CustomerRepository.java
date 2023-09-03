4ff0895d9d10076e739a535c8e5acd431fe8b235
Amador <amasabcar@outlook.com>
2023-09-03 19:31:18 +0200

    added repo annotation

--- src/main/java/com/project/customer/CustomerRepository.java
+++ src/main/java/com/project/customer/CustomerRepository.java
@@ -1,6 +1,8 @@
 package com.project.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface  CustomerRepository extends JpaRepository<Customer, Long> {
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
